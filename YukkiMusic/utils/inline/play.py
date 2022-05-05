1fa61f7893403c2b3aa4f1c2733cdcb9b453a554
Rizky_Yudha <rizkyyudha0023@gmail.com>
2022-05-06 05:33:58 +0700

    last

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -8,6 +8,7 @@
 # All rights reserved.
 
 from pyrogram.types import InlineKeyboardButton
+from config import SUPPORT_CHANNEL'
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
@@ -36,8 +37,7 @@ def stream_markup(_, videoid):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["PL_B_2"],
-                callback_data=f"add_playlist {videoid}",
+                text=_["PL_B_2"], url=f"{SUPPORT_CHANNEL}",
             ),
             InlineKeyboardButton(
                 text=_["PL_B_3"], switch_inline_query_current_chat=""
