072b03c7317fd8312b106d91a48a86430a950501
Rizky_Yudha <rizkyyudha0023@gmail.com>
2022-05-06 04:20:17 +0700

    try

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -29,9 +29,6 @@ from YukkiMusic.utils.database import (add_served_chat,
                                        get_userss, is_on_off,
                                        is_served_private_chat)
 from YukkiMusic.utils.decorators.language import LanguageStart
-from YukkiMusic.utils.inline import (help_pannel, private_panel,
-                                     start_pannel)
-
 loop = asyncio.get_running_loop()
 
 
