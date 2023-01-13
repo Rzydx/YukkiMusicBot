1af1e91d5282e5cbb4c46ff287c8978a5f7a45d5
Rizky_Yudha <102364151+Rzydx@users.noreply.github.com>
2023-01-13 07:36:25 +0700

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -76,7 +76,7 @@ async def activevi_(_, message: Message):
             disable_web_page_preview=True,
         )
 @app.on_message(filters.command("aht", [".", "^", "-", "!", "/"]) & SUDOERS)
-async def activecilik(_, message: Message):
+async def activevi_(_, message: Message):
     ms = len(await get_active_chats())
     vd = len(await get_active_video_chats())
     await app.send_message(message.chat.id, 
